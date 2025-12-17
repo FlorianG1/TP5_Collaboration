fd6feb42039eb84c7807b9aaebd590b3806b16d6
Florian Ginestet <ginestet.florian@gmail.com>
2025-12-17 14:34:00 +0100

    Ajout de la classe Rotation

--- rotation.cpp
+++ rotation.cpp
@@ -1,6 +1,16 @@
 #include "rotation.h"
+#include <iostream>
+using namespace std;
 
-Rotation::Rotation()
-{
+Rotation::Rotation(int p_angle) {
+    m_angle = p_angle;
+}
 
+void Rotation::executer(ContexteRobot& ctx) {
+    afficherNom();
+    ctx.rotation(m_angle);
+}
+
+void Rotation::afficherNom() const {
+    cout << "Action : Rotation " << m_angle << " degres"<< endl;
 }
--- rotation.h
+++ rotation.h
@@ -1,11 +1,16 @@
 #ifndef ROTATION_H
 #define ROTATION_H
+#include "actionrobot.h"
+#include "contexterobot.h"
 
-
-class Rotation
+class Rotation : public ActionRobot
 {
+private:
+    int m_angle;
 public:
-    Rotation();
+    Rotation(int p_angle);
+    void executer(ContexteRobot& ctx) override;
+    void afficherNom() const override;
 };
 
 #endif // ROTATION_H
