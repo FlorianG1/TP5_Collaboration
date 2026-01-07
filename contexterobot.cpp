ab4e0e2d61c54cfa44e37aab928a2fb0d4a53dc4
Florian Ginestet <ginestet.florian@gmail.com>
2026-01-07 17:41:14 +0100

    Amélioration de l'affichage du journal

--- contexterobot.cpp
+++ contexterobot.cpp
@@ -60,7 +60,7 @@ void ContexteRobot::rotation(int p_angle) {
 
 // Affichage
 void ContexteRobot::afficherPosition() {
-    std::cout << "Position : (" << m_x << ", " << m_y << ", " << m_z << ")";
-    std::cout << " | Pince " << (m_pince_ouverte ? "ouverte" : "fermee");
-    std::cout << " | Angle : " << m_angle_robot << " degres " << std::endl;
+    std::cout << "Position : X="<< m_x << " mm, Y=" << m_y << " mm, Z=" << m_z <<" mm\n";
+    std::cout << "Pince    : " << (m_pince_ouverte ? "ouverte" : "fermee\n");
+    std::cout << "Angle    : " << m_angle_robot << " degres " << std::endl;
 }
